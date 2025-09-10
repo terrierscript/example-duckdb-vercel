6fd223e304c64165c33f19874949b1b736377d5b
S. Suzuki <terrierscript@gmail.com>
2025-09-10 20:30:22 +0900

    add

--- src/lib/duckdb.ts
+++ src/lib/duckdb.ts
@@ -8,6 +8,9 @@ export const querySample = async () => {
 
   const instance = await DuckDBInstance.create()
   const conn = await instance.connect()
+  await conn.run(`SET home_directory = '/tmp';`)
+  await conn.run(`INSTALL httpfs; `)
+  await conn.run(`LOAD httpfs; `)
   const url = `https://example-duckdb-vercel.vercel.app/example/csv`
   const response = await conn.run(`SELECT * FROM read_csv('${url}')`)
   return response.getRowsJS()
