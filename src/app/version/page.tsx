079960c8aa74c35130bea02ffa1bf258a12c6975
S. Suzuki <terrierscript@gmail.com>
2025-09-10 20:27:18 +0900

    add

--- src/app/version/page.tsx
+++ src/app/version/page.tsx
@@ -1,14 +1,17 @@
 "use server"
-import { getVersion } from "../../lib/duckdb"
+import { getVersion, querySample } from "../../lib/duckdb"
 
 
 
 const Page = async () => {
   const version = await getVersion()
+  const result = await querySample()
   return (
     <div>
       <h1>DuckDB Version</h1>
       <p>{version}</p>
+      <h2>Query Sample</h2>
+      <pre>{JSON.stringify(result, null, 2)}</pre>
     </div>
   )
 }
--- src/lib/duckdb.ts
+++ src/lib/duckdb.ts
@@ -1,6 +1,14 @@
-import duckdb from '@duckdb/node-api'
+import duckdb, { DuckDBInstance } from '@duckdb/node-api'
 
 export const getVersion = () => {
   return duckdb.version()
 }
 
+export const querySample = async () => {
+
+  const instance = await DuckDBInstance.create()
+  const conn = await instance.connect()
+  const url = `https://example-duckdb-vercel.vercel.app/example/csv`
+  const response = await conn.run(`SELECT * FROM read_csv('${url}')`)
+  return response.getRowsJS()
+}
